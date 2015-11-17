09cc6701d5fdfbc3e026d4b18b6ca7755ffdeaa8
Karl Soulière-Crépeau <karlysun@gmail.com>
2015-11-17 08:15:56 +0000

    Menu is working, added url effect, modified some CSS

--- js/app-bundle.js
+++ js/app-bundle.js
@@ -46,6 +46,7 @@
 
 	$(document).foundation();
 
+	//// TYPEWRTITER EFFECT
 	var captionLength = 0;
 	var caption = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan.";
 
@@ -57,6 +58,7 @@
 	    setTimeout('type()', 60);
 	}
 
+	//// BLINKING CURSOR
 	function cursorAnimation() {
 	    $('#cursor').animate({
 	        opacity: 0
@@ -66,34 +68,53 @@
 	}
 
 	//// SCROLL EFFECT
-	$('a[href*=#]:not([href=#])').click(function() {
+	var automaticScroll = false;
 
+	$('a[href*=#]:not([href=#])').click(function() {
 	    if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
 	        var target = $(this.hash);
 	        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
 	        if (target.length) {
+	            $(".overlay").fadeOut(100);
+	            $("#menu").fadeOut("fast");
+	            automaticScroll = true;
 	            $('html,body').animate({
 	                scrollTop: target.offset().top
-	            }, 1000);
+	            }, 1000, function(){
+	                automaticScroll = false;
+	            });
 	            return false;
 	        }
 	    }
 	});
 
-	var scrollOrigin = $(window).scrollTop();
-
-	$(window).on("scroll", function(){
-	    
-	    var scrollPos = $(window).scrollTop();
-	    
-	    if(scrollOrigin > scrollPos){
-	        $("#menu").animate({top: "0"}, "fast");
+	//// MENU EFFECT
+	var lastScrollTop = 0;
+	$(window).scroll(function(event){
+	    var st = $(this).scrollTop();
+	    if($(window).scrollTop() > $("header").height()){
+	        if (st < lastScrollTop && automaticScroll === false){
+	            $("#menu").fadeIn("fast");
+	        } else {
+	            $("#menu").fadeOut("fast");
+	        }
 	    }
 	    else {
-	        $("#menu").slideUp("fast");
+	        $("#menu").hide();
 	    }
-	    
-	    scrollOrigin = scrollPos;
+	    lastScrollTop = st;
+	});
+
+	//// OVERLAY
+	$('.material-icons').on('click', function() {
+
+	    $(".overlay").fadeIn(100).css("display","flex");
+
+	    $(document).on('click','.overlay, .overlay img', function(evt) {
+	        if (evt.target === evt.currentTarget) {
+	            $(".overlay").fadeOut(100);
+	        }
+	    });
 	});
 
 /***/ }
